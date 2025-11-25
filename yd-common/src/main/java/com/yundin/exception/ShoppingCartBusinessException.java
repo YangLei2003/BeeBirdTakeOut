cd389e0d51dfec437ff104c8209b4e9b23a33051
YangLei_2003 <2964372477@qq.com>
2025-11-25 10:10:01 +0000

    删除文件 yd-common

--- yd-common/src/main/java/com/yundin/exception/ShoppingCartBusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.yundin.exception;
-
-public class ShoppingCartBusinessException extends BaseException {
-
-    public ShoppingCartBusinessException(String msg) {
-        super(msg);
-    }
-
-}
